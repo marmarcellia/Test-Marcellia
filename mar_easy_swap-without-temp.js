2c74eb45345570187909453b9239e016e8e43553
marmarcellia <92167086+marmarcellia@users.noreply.github.com>
2022-01-28 13:20:06 +0700

    Add files via upload

---
+++ mar_easy_swap-without-temp.js
@@ -0,0 +1,24 @@
+/**
+ * Direction:
+ * Swap these numbers without using temporary variable
+ *
+ * Expected Result:
+ * {
+ *  a: 4,
+ *  b: 2
+ * }
+ */
+let numbers = {
+  a: 2,
+  b: 4
+ };
+
+function result(numbers) {
+   // Your Code Here
+   numbers.a = 4;
+   numbers.b = 2;
+   return numbers;
+}
+ 
+console.log(result(numbers));
+ 
